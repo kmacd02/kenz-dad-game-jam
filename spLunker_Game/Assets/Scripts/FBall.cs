c088864f5aa20a6b73a323aa8ed98b15f6b6f9d5
Douger5 <109234934+Douger5@users.noreply.github.com>
2022-07-16 21:40:11 -0400

    Damage Enemy

--- spLunker_Game/Assets/Scripts/FBall.cs
+++ spLunker_Game/Assets/Scripts/FBall.cs
@@ -6,10 +6,20 @@ public class FBall : MonoBehaviour
 {
     public float speed = 20f;
     public Rigidbody2D rb;
+    public int damage = 25;
 
     // Start is called before the first frame update
     void Start()
     {
         rb.velocity = transform.right * speed;
+    }
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        EnemyStats enemy = hitInfo.GetComponent<EnemyStats>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+        Destroy(gameObject);
     }
 }
